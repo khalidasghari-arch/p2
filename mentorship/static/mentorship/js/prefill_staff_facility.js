b6fc4d17dec187c01e914044ce012c7209bf10ae
Ahmad Khalid Asghari <khalid.asghari@gmail.com>
2026-01-29 16:33:08 +0430

    Final stable topic refresh logic 2nd round not okay

--- mentorship/static/mentorship/js/prefill_staff_facility.js
+++ mentorship/static/mentorship/js/prefill_staff_facility.js
@@ -1,38 +1,21 @@
 (function ($) {
   $(document).ready(function () {
 
-    function getFacilityId() {
-      return $("#id_facilityfk").val();
+    // This file should NEVER crash
+    // Only run if popup exists
+    var isPopup = window.location.search.indexOf("_popup=1") !== -1;
+    if (!isPopup) return;
+
+    // Facility is passed as ?facility=<id>
+    var params = new URLSearchParams(window.location.search);
+    var facilityId = params.get("facility");
+
+    if (!facilityId) return;
+
+    var $facilitySelect = $("select[name='hfname']");
+    if ($facilitySelect.length) {
+      $facilitySelect.val(facilityId);
     }
-
-    function addFacilityParamToHref(href, facId) {
-      try {
-        var url = new URL(href, window.location.origin);
-        url.searchParams.set("facility", facId);
-        return url.pathname + url.search;
-      } catch (e) {
-        // fallback (very rare)
-        if (href.indexOf("facility=") !== -1) return href;
-        return href + (href.indexOf("?") === -1 ? "?" : "&") + "facility=" + encodeURIComponent(facId);
-      }
-    }
-
-    // Update the add popup link when user clicks "+" (works even if DOM changes)
-    $(document).on("mousedown click", "a.related-widget-wrapper-link.add-related", function () {
-      var facId = getFacilityId();
-      if (!facId) return;
-
-      // Only apply to the menteename field "+" (so we don't affect other add popups)
-      // In tabular inline, the select is inside td.field-menteename
-      var isMenteeField = $(this).closest("td.field-menteename, .form-row.field-menteename").length > 0;
-      if (!isMenteeField) return;
-
-      var href = $(this).attr("href");
-      if (!href) return;
-
-      var newHref = addFacilityParamToHref(href, facId);
-      $(this).attr("href", newHref);
-    });
 
   });
 })(django.jQuery);
