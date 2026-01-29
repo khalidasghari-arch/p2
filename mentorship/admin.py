d06986ee4a09d11b4b8b2f99b525f8b467c789a3
Ahmad Khalid Asghari <khalid.asghari@gmail.com>
2026-01-29 22:05:03 +0430

    Force mentorship topic ajax refresh

--- mentorship/admin.py
+++ mentorship/admin.py
@@ -265,7 +265,7 @@ class MentorshipvisitAdmin(ProvinceRestrictedAdminMixin, admin.ModelAdmin):
 
     class Media:
         js = (
-            "mentorship/js/prefill_staff_facility.js",
+            # "mentorship/js/prefill_staff_facility.js",
             "mentorship/js/topic_refresh_stable.js",
         )
 
